49a96ed0e6069c0d277c80d62a92072c2b04ee5d
Sergii Butenko <sergii.butenko@globallogic.com>
2023-03-14 18:13:11 +0100

    added config

---
+++ src/config/config.py
@@ -0,0 +1,24 @@
+class Config:
+    """
+    
+    """
+
+
+    def register(self, name):
+        """
+        Register name of the key which is used 
+        in tests
+        """
+        pass
+
+    def set(self, key, value):
+        """
+        Set key to value
+        """
+        pass
+
+    def get(self):
+        """
+        Return existing value
+        """
+        pass
