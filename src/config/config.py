b77695965989991e5e14a9e48316c3a2684f95eb
Sergii Butenko <sergii.butenko@globallogic.com>
2023-03-16 13:50:39 +0100

    created config

--- src/config/config.py
+++ src/config/config.py
@@ -1,24 +1,53 @@
-class Config:
-    """
-    
-    """
+import os
 
+from src.config.providers.config_from_env_provider import ConfigFromEnvProvider
+from src.config.providers.config_from_json_provider import ConfigFromSimpleJsonProvider
+
+
+class Config:
+    default_env = "dev"
+
+    def __init__(self) -> None:
+        self.conf_dict = {}
+
+        target = os.environ.get('TARGET')
+        if target is None:
+            target = Config.default_env
+
+        json_path = f"src/config/env_configs/{target}.json"
+
+        self.providers = [
+            ConfigFromSimpleJsonProvider(json_path),
+            ConfigFromEnvProvider(),
+            ]
+
+        self.register("BASE_URL")
 
     def register(self, name):
         """
         Register name of the key which is used 
         in tests
         """
-        pass
 
-    def set(self, key, value):
-        """
-        Set key to value
-        """
-        pass
+        # Order in self.provider makes difference 
+        for provider in self.providers:
+            val = provider.get(name)
 
-    def get(self):
+            if val is not None:
+                self.conf_dict[name] = val
+
+        # raise error if no value is found across the providers
+        if self.conf_dict[name] is None:    
+            raise Exception(f"{name} variable is not set in config")
+
+        print(f"{name} variable is registered in config with value {self.conf_dict[name]}")
+
+    def get(self, name):
         """
         Return existing value
         """
-        pass
+        return self.conf_dict[name]
+
+
+# python way singleton
+config = Config()
--- tests/test_test.py
+++ tests/test_test.py
@@ -1,6 +1,12 @@
+from src.config.config import config
+
 def test_user_age_is_43(user):
     assert user.age == 43
 
 
 def test_user_age_is_50(user):
     assert user.age == 50
+
+
+def test_config():
+    print(config.get("BASE_URL"))
